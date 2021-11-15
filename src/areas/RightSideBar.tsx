42832d104fd7ee100bf44209bb56fe04da5559fa
Szymon Kucharski <speckope@gmail.com>
2021-11-15 20:10:25 +0100

    Collapsing well, working as intended, logic only in right sidebar, but hamburger is buggy, I do not know how to fix it.

--- src/areas/RightSideBar.tsx
+++ src/areas/RightSideBar.tsx
@@ -5,12 +5,24 @@ import { cvar } from 'src/theming/cvar';
 import styled from 'styled-components';
 import { ReactComponent as MessageSVG } from '../assets/top-area/message-icon.svg';
 import { ReactComponent as NotificationsSVG } from '../assets/top-area/notifications-icon.svg';
+import Hamburger from 'hamburger-react';
+import { useState } from 'react';
 
 interface RightSideBarProps {}
 
 const RightSideBar: React.FC<RightSideBarProps> = () => {
+  const [isRightBurgirOpen, setIsRightBurgirOpen] = useState(false);
+
   return (
-    <Wrapper>
+    <RightSideBarWrapper isRightBurgirOpen={isRightBurgirOpen}>
+      <Hamburger
+        size={25}
+        direction='left'
+        color={cvar('colorPrimary')}
+        label='Show right sidebar'
+        toggled={isRightBurgirOpen}
+        toggle={setIsRightBurgirOpen}
+      />
       <TopIconsWrapper>
         <SmallButton clickable>
           <MessageSVG />
@@ -24,11 +36,13 @@ const RightSideBar: React.FC<RightSideBarProps> = () => {
         <CreditCard />
         <BarChart />
       </CardBarChartWrapper>
-    </Wrapper>
+    </RightSideBarWrapper>
   );
 };
 
-const Wrapper = styled.div`
+const RightSideBarWrapper = styled.div<{
+  isRightBurgirOpen: boolean;
+}>`
   width: 33%;
   max-width: 48rem;
 
@@ -44,6 +58,49 @@ const Wrapper = styled.div`
   padding-left: 4rem;
   padding-right: 6rem;
   padding-bottom: 4rem;
+
+  .hamburger-react {
+    position: absolute;
+
+    z-index: 30;
+
+    display: none;
+
+    top: 1rem;
+    right: 0.5rem;
+  }
+
+  @media (max-width: 1150px) {
+    ${(props) =>
+      props.isRightBurgirOpen
+        ? `
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100vh;
+        width: 100vw;
+        padding-top: 0;
+        padding-right: 6rem;
+
+        .hamburger-react {
+          top: 1rem;
+          right: -5rem;
+        }
+        `
+        : `
+      * {
+        display: none;
+      };
+      width: 6rem;
+      padding: 0;
+    `}
+
+    // Without .hamburger-react *  inside elements (burger itself is invisible)
+    .hamburger-react,
+    .hamburger-react * {
+      display: inline;
+    }
+  }
 `;
 
 const CardBarChartWrapper = styled.div`
